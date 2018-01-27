177323289a12d3623ab5dfa8628fdaf50f0b73da
Greg <gregharanczyk@gregs-macbook-pro.local>
2018-01-27 15:14:13 -0500

    Added ability to add multiple items to list using comma seperation

--- passDataBetweenViews/ToDoController.swift
+++ passDataBetweenViews/ToDoController.swift
@@ -15,7 +15,11 @@ class ToDoController: NSObject {
     static var toDoArray: [String] = []
     
     class func addToDo(newToDo: String) {
-        ToDoController.toDoArray.append(newToDo)
+        if newToDo.contains(","){
+            ToDoController.insertMultipleStrings(input: newToDo)
+        } else {
+            ToDoController.toDoArray.append(newToDo)
+        }
     }
     
     class func removeToDo(atIndex: Int){
@@ -23,6 +27,18 @@ class ToDoController: NSObject {
             return
         } else {
             ToDoController.toDoArray.remove(at: atIndex)
+        }
+    }
+    
+    private class func insertMultipleStrings(input: String){
+        let splitArray = input.split(separator: ",")
+        for element in splitArray {
+            //Prevent addition of blank values to list
+            if element == " " {
+                continue
+            }
+            //Remove leading whitespace
+            ToDoController.toDoArray.append(String(element.trimmingCharacters(in: .whitespaces)))
         }
     }
 }
